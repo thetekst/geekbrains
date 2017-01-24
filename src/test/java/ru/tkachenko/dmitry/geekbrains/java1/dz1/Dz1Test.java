1c1f71d4af3215816bb2f7d17926de4331b931d2
Dmitry Tkachenko <thetekst@gmail.com>
2017-01-24 21:00:50 +0300

    correction

--- src/test/java/ru/tkachenko/dmitry/geekbrains/java1/dz1/Dz1Test.java
+++ src/test/java/ru/tkachenko/dmitry/geekbrains/java1/dz1/Dz1Test.java
@@ -1,47 +1,22 @@
 package ru.tkachenko.dmitry.geekbrains.java1.dz1;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static ru.tkachenko.dmitry.geekbrains.java1.dz1.Dz1.*;
+
 /**
- * Created by dmitry tkachenko on 1/23/17.
+ * Created by dmitry tkachenko on 1/24/17.
  */
-public class Dz1 {
-    public static void main(String[] args) {
-
-//        step 2
-        task2();
-
-//        step 3
-        double result = calc(1, 3, 5, 2);
-        System.out.printf("step 3: %f\n",result);
-
-//        step 4
-        System.out.printf("step 4: %s\n", range(1,8));
-
-//        step 5
-        evenOrOdd(-2);
-
-//        step 6
-        System.out.printf("step 6: %s\n",isNegative(-1));
-
-//        step 7
-        printName("dima");
-
-//        step 8
-        isLeapYear(2000);
-    }
+public class Dz1Test {
 
     /**
      * step 2
      * Создать переменные всех пройденных типов данных, и инициализировать их значения;
      */
-    public static void task2() {
-        byte b = 127;
-        short st = 12419;
-        char ch = 'A';
-        int integer = 24;
-        long lg = 124124L;
-        float fl = 12.345949f;
-        double dl = 4323.45345;
-        boolean bool = true;
+    @Test
+    public void task2Test() {
+        task2();
     }
 
     /**
@@ -49,8 +24,14 @@ public class Dz1 {
      * Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
      * где a, b, c, d – входные параметры этого метода;
      */
-    public static double calc(double a, double b, double c, double d) {
-        return a * (b + (c / d));
+    @Test
+    public void calcTest() {
+        double delta = 0;
+        double result1 = calc(1, 3, 5, 2);
+        double result2 = calc(45.3, 10.0, 5.12, 9);
+
+        assertEquals(5.5, result1, delta);
+        assertEquals(478.7706666666666, result2, delta);
     }
 
     /**
@@ -58,23 +39,25 @@ public class Dz1 {
      * Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в
      * пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
      */
-    public static boolean range(int one, int two) {
-        int sum = one + two;
-        if (sum > 9 && sum < 21) return true;
-        return false;
+    @Test
+    public void rangeTest() {
+        boolean resultFalse = range(1, 8);
+        boolean resultTrue = range(6, 8);
+        assertEquals(false, resultFalse);
+        assertEquals(true, resultTrue);
     }
 
     /**
      * step 5
      * Написать метод, которому в качестве параметра передается целое число, метод должен
-     * напечатать в консоль положительное ли число передали, или отрицательное; Замечание:
-     * ноль считаем положительным числом
+     * напечатать в консоль положительное ли число передали, или отрицательное;
+     * Замечание: ноль считаем положительным числом
      */
-    public static void evenOrOdd(int num) {
-        String word;
-        if ((num % 2) == 0) word = "even";
-        else word = "odd";
-        System.out.printf("step 5: %s\n", word);
+    @Test
+    public void printPositiveOrNegativeTest() {
+        printPositiveOrNegative(3);
+        printPositiveOrNegative(0);
+        printPositiveOrNegative(-2);
     }
 
     /**
@@ -82,9 +65,13 @@ public class Dz1 {
      * Написать метод, которому в качестве параметра передается целое число, метод должен
      * вернуть true, если число отрицательное;
      */
-    public static boolean isNegative(int num) {
-        if (num < 0) return true;
-        return false;
+    @Test
+    public void isNegativeTest() {
+        boolean positive = isNegative(0);
+        boolean negative = isNegative(-1);
+
+        assertEquals(false, positive);
+        assertEquals(true, negative);
     }
 
     /**
@@ -92,8 +79,9 @@ public class Dz1 {
      * Написать метод, которому в качестве параметра передается строка, обозначающая имя,
      * метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
      */
-    public static void printName(String name) {
-        System.out.printf("step 7: Привет, %s!\n", name);
+    @Test
+    public void printNameTest() {
+        printName("dima");
     }
 
     /**
@@ -102,7 +90,11 @@ public class Dz1 {
      * консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й –
      * високосный.
      */
-    public static void isLeapYear(int year) {
-        System.out.printf("step 8: \n");
+    @Test
+    public void printYearTest() {
+        printYear(2020);
+        printYear(2000);
+        printYear(2016);
+        printYear(2005);
     }
 }
