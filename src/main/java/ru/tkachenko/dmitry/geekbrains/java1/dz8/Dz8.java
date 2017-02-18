9e82bcd66110a63eb215a51818e57c312996de28
Dmitry Tkachenko <thetekst@gmail.com>
2017-02-18 12:26:09 +0300

    Dz8 start

---
+++ src/main/java/ru/tkachenko/dmitry/geekbrains/java1/dz8/Dz8.java
@@ -0,0 +1,16 @@
+package ru.tkachenko.dmitry.geekbrains.java1.dz8;
+
+import ru.tkachenko.dmitry.geekbrains.java1.dz8.gui.frame.Window;
+
+import javax.swing.*;
+
+/**
+ * Created by dmitry tkachenko on 2/18/17.
+ * <p>
+ * Сделать крестики-нолики 3x3 с использованием Swing
+ */
+public class Dz8 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Window::new);
+    }
+}
---
+++ src/main/java/ru/tkachenko/dmitry/geekbrains/java1/dz8/gui/panel/Panel.java
@@ -0,0 +1,14 @@
+package ru.tkachenko.dmitry.geekbrains.java1.dz8.gui.panel;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+/**
+ * Created by dmitry tkachenko on 2/18/17.
+ */
+public class Panel extends JPanel {
+    public Panel() {
+        setBorder(new LineBorder(Color.black, 2));
+    }
+}
