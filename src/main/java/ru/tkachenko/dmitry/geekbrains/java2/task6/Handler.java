21faccdcb4086842afbc929182808b82e958545f
Dmitry Tkachenko <thetekst@gmail.com>
2017-03-13 16:50:49 +0300

    java 2 task 6 Handler refactoring

--- src/main/java/ru/tkachenko/dmitry/geekbrains/java2/task6/Handler.java
+++ src/main/java/ru/tkachenko/dmitry/geekbrains/java2/task6/Handler.java
@@ -16,23 +16,23 @@ public class Handler {
 
     public Handler(Socket socket, String className) {
 
-        try (Scanner sin = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in);
              DataInputStream in = new DataInputStream(socket.getInputStream());
              DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
-            String clientMsg;
-            Sender serverSender = new Sender(sin, out);
-            Thread senderThread = new Thread(serverSender);
+            String msg;
+            Sender sender = new Sender(scanner, out);
+            Thread senderThread = new Thread(sender);
             senderThread.start();
 
             while (true) {
-                clientMsg = in.readUTF();
-                if (clientMsg.equals("bye")) {
-                    System.out.printf("%s stop write thread%n", className);
-                    serverSender.setFlag(false);
+                msg = in.readUTF();
+                if (msg.equals("bye")) {
+                    System.out.printf("%s stop writing%n", className);
+                    sender.setFlag(false);
                     break;
                 }
-                System.out.printf("%s says: %s%n", className, clientMsg);
+                System.out.printf("%s says: %s%n", className, msg);
             }
         } catch (IOException e) {
             e.printStackTrace();
