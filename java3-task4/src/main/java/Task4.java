bd9a56891146e4a8461e809ed0fa145fb0491194
Dmitry <thetekst@gmail.com>
2017-06-03 01:59:55 +0300

    create java 3 task 4. 1,2 has done

--- java3-task4/src/main/java/Task4.java
+++ java3-task4/src/main/java/Task4.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * @author Dmitry Tkachenko
  * @version 1.0 6/2/17
@@ -18,6 +20,119 @@
 */
 public class Task4 {
 
+    private final Object monitor;
+    private volatile char letter = 'A';
+    private static final String FILE_PATH = "java3-task4/src/main/resources";
+
+    public Task4() {
+        this.monitor = new Object();
+    }
+
+    public void printA() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (letter != 'A') {
+                        monitor.wait();
+                    }
+
+                    System.out.print('A');
+                    letter = 'B';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (letter != 'B') {
+                        monitor.wait();
+                    }
+
+                    System.out.print('B');
+                    letter = 'C';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (letter != 'C') {
+                        monitor.wait();
+                    }
+
+                    System.out.print('C');
+                    letter = 'A';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void runTask1() {
+        Task4 task4 = new Task4();
+
+        new Thread(task4::printA, "one").start();
+        new Thread(task4::printB, "two").start();
+        new Thread(task4::printC, "three").start();
+    }
+
+    public void test(char ch) {
+        try {
+            for (int i = 0; i < 5; i++) {
+                try (FileWriter fw = new FileWriter(FILE_PATH + "/readme.txt", true); BufferedWriter bw =
+                        new BufferedWriter(fw);
+                     PrintWriter out = new PrintWriter(bw)) {
+
+                    for (int j = 0; j < 10; j++) {
+                        out.print(ch);
+                    }
+
+                    out.println();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Thread.sleep(20);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void runTask2() {
+        Task4 task4 = new Task4();
+
+        Thread t1 = new Thread(() -> task4.test('z'));
+        Thread t2 = new Thread(() -> task4.test('r'));
+        Thread t3 = new Thread(() -> task4.test('d'));
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    private static void runTask3() {
+        // TODO
+    }
+
     public static void main(String[] args) {
+//        runTask1();
+//        runTask2();
+        runTask3();
     }
 }
